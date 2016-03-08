1cc16a5e8fb3ec7e44e63b5cc40a818ecfb5570d
Shubhi Jain <shubhijain@caltech.edu>
2016-03-07 23:24:26 -0800

    Adding config file

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import os
+
 from flask import Flask
 
+
+################################### CONFIG #####################################
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
+################################### ROUTES #####################################
 @app.route('/')
 def index():
     return "Hello World!"
