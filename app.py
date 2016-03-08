3b44dd7255b1addda5244cb6d3a669f46413de51
Shubhi Jain <shubhijain@caltech.edu>
2016-03-08 00:05:38 -0800

    Base template

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+import requests
 
 from flask import Flask
+from flask import render_template
+from flask import request
 
 
 ################################### CONFIG #####################################
@@ -11,7 +14,22 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 ################################### ROUTES #####################################
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/eat', methods=['GET', 'POST'])
+def eat():
+    req = dict(request.form)
+    args = dict(request.args)
+    # POST Request from form
+    if request.method == 'POST':
+        if 'location' in req:
+            loc = req['location']
+    # API GET Request
+    else:
+        if 'location' in args:
+            loc = args['location']
+    return 'hi'
+
 
 if __name__ == '__main__':
     app.run()
